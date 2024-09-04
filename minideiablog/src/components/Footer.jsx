c61b9af397c74cdb9ecd3ce93c3a8d142059c8b5
mariaeduardamassotte <maria.massotte@etec.sp.gov.br>
2024-09-04 20:40:58 -0300

    Aula 0409 Estrutura de projeto react

---
+++ minideiablog/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from './Footer.module.css'
+
+const Footer = () => {
+  return (
+   <>
+   <fotter>
+    <h3>Idéias de quem gosta de ficar com o pé na estrada!</h3>
+    <p>IdeiaApp &copy; 2024</p>
+   </fotter>
+   </>
+  )
+}
+
+export default Footer
